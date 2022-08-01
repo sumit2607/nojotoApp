5fb55f881ccab88b5be15c10ab5b5d48a00540b7
sumit <developer2607@gmail.com>
2022-08-01 12:19:50 +0530

    Camera integrated successfully

---
+++ app/src/main/java/com/example/nojotoapp/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.nojotoapp
+
+import android.Manifest
+
+object Constants {
+
+    const val REQUEST_CODE_PERMISSIONS = 123
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+}
--- app/src/main/java/com/example/nojotoapp/ui/CameraFragment.kt
+++ app/src/main/java/com/example/nojotoapp/ui/CameraFragment.kt
@@ -1,22 +1,98 @@
 package com.example.nojotoapp.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.nojotoapp.Constants
 import com.example.nojotoapp.R
+import kotlinx.android.synthetic.main.fragment_camera.*
 
 
-class CameraFragment : Fragment() {
+class CameraFragment : Fragment(R.layout.fragment_camera) {
 
+    private var imageCapture: ImageCapture? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(context, "This feature is not implemented properly", Toast.LENGTH_LONG)
+            .show()
+
+        if (allPermissionsGranted()) {
+
+            startCamera()
+
+        } else {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(context, "Permission not granted", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+    private fun allPermissionsGranted() = Constants.REQUIRED_PERMISSIONS.all {
+        context?.let { it1 ->
+            ContextCompat.checkSelfPermission(
+                it1,
+                it
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+
+        val cameraProvideFeature = context?.let { ProcessCameraProvider.getInstance(it) }
+
+        cameraProvideFeature?.addListener({
+
+            val cameraProvider: ProcessCameraProvider = cameraProvideFeature.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            cameraProvider.unbindAll()
+
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageCapture
+            )
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+}
